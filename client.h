ace20d7ddd1740ca626e34b1c6c64d5636f95130
uidq0655 <Xiaocheng.Su@desay-svautomotive.com>
2017-12-29 09:27:51 +0800

    add comment

--- client.h
+++ client.h
@@ -9,8 +9,28 @@
 #define TRUE 1
 #endif
 
+/**
+ * msg_thread_init - init two background thread to send and receive data.
+ * @addr:	internet address to connect.
+ * @return:	0:suecess, 1: fail
+ * Description: Used to connect to socket.
+ */
 int msg_thread_init(void *addr);
+
+/**
+ * receive_from_server - used to receive msg from server. Buf size
+ * is at least 2048
+ * @buf:	
+ * @return:	-1: fail, >0: number of bytes get from server.
+ * Description: Used to connect to socket.
+ */
 int receive_from_server(char *buf);
+/**
+ * send_to_server - send data to server
+ * @buf:	buffer used to send data
+ * @length:	buffer length, should not exceed 2044
+ * @return:	0:suecess, -1: fail
+ */
 int send_to_server(char *buf, int length);
 
 #endif
